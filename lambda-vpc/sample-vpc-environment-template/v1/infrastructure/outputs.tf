4ccca00c5a6214187b35b4028b5209f8569cf7d4
warpspeed6 <25733040+warpspeed6@users.noreply.github.com>
2022-10-24 15:56:53 +0200

    Update outputs.tf

--- lambda-vpc/sample-vpc-environment-template/v1/infrastructure/outputs.tf
+++ lambda-vpc/sample-vpc-environment-template/v1/infrastructure/outputs.tf
@@ -1,11 +1,9 @@
-output "vpc_arn" {
-  value = module.vpc.vpc_arn
+output "private_subnet_id" {
+  value          = [for az, subnet in aws_subnet.private: subnet.id]
 }
-
-output "subnet_id" {
-  value = one(module.vpc.private_subnets) # there is a known issue with terraform lists as outputs for proton
+output "public_subnet_id" {
+  value          = [for az, subnet in aws_subnet.public: subnet.id]
 }
-
 output "security_group_id" {
   value = module.vpc.default_security_group_id
 }
