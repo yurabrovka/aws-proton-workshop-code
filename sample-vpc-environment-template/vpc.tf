671802b6bade61fca0c20050e9746ecf5833c021
warpspeed6 <25733040+warpspeed6@users.noreply.github.com>
2022-10-24 15:47:51 +0200

    Update vpc.tf

--- sample-vpc-environment-template/vpc.tf
+++ sample-vpc-environment-template/vpc.tf
@@ -14,7 +14,8 @@ module "vpc" {
 
   azs             = ["${var.aws_region}b"]
   private_subnets = ["10.0.1.0/24"]
-
+  public_subnets  = ["10.0.101.0/24"]
+  
   enable_nat_gateway                             = true
   enable_vpn_gateway                             = false
   enable_ipv6                                    = true
